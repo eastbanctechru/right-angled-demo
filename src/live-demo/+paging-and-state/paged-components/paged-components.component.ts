f5d0f13ad91098323fa6510ecbf5629a12034de8
Fedor Shchudlo <fshchudlo@gmail.com>
2016-09-23 14:01:50 +0700

    feat(demo): stub pagers

--- src/live-demo/+paging-and-state/paged-components/paged-components.component.ts
+++ src/live-demo/+paging-and-state/paged-components/paged-components.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { ListComponent } from 'right-angled';
 
-import { AirportsListRequest, AirportsService } from '../../shared';
+import { AirportsPagedListRequest, AirportsService } from '../../shared';
 
 @Component({
     selector: 'rt-demo-paged-components',
@@ -11,7 +11,7 @@ export class PagedComponentsComponent {
     @ViewChild(ListComponent) public listComponent: ListComponent;
     constructor(public airportsService: AirportsService) {
     }
-    public loadData = (requestParams: AirportsListRequest): any => {
-        return this.airportsService.getAirportsList(requestParams, 700);
+    public loadData = (requestParams: AirportsPagedListRequest): any => {
+        return this.airportsService.getAirportsPaged(requestParams, 700);
     };
 }
