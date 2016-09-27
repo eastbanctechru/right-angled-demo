f5973805f35c51718d73d78449a3cae5a3c701c8
Fedor Shchudlo <fshchudlo@gmail.com>
2016-09-27 10:21:37 +0700

    feat(demo): reusable controls demo

--- src/live-demo/+paging-and-state/paging-and-state.module.ts
+++ src/live-demo/+paging-and-state/paging-and-state.module.ts
@@ -6,12 +6,12 @@ import { BufferedComponentsComponent } from './buffered-components/buffered-comp
 import { PagedComponentsComponent } from './paged-components/paged-components.component';
 import { PagingAndStateComponent } from './paging-and-state.component';
 import { ReusableBufferedListSampleComponent } from './reusable-buffered-sample/reusable-buffered-sample.component';
-import { ReusableComponentComponent } from './reusable-component/reusable-component.component';
+import { ReusablePagedListSampleComponent } from './reusable-paged-sample/reusable-paged-sample.component';
 import { StateComponentsComponent } from './state-components/state-components.component';
 
 @NgModule({
-    declarations: [BufferedComponentsComponent, ReusableBufferedListSampleComponent, ReusableComponentComponent, PagedComponentsComponent, PagingAndStateComponent, StateComponentsComponent],
-    exports: [BufferedComponentsComponent, ReusableBufferedListSampleComponent, ReusableComponentComponent, PagedComponentsComponent, PagingAndStateComponent, StateComponentsComponent],
+    declarations: [BufferedComponentsComponent, ReusableBufferedListSampleComponent, ReusablePagedListSampleComponent, PagedComponentsComponent, PagingAndStateComponent, StateComponentsComponent],
+    exports: [BufferedComponentsComponent, ReusableBufferedListSampleComponent, ReusablePagedListSampleComponent, PagedComponentsComponent, PagingAndStateComponent, StateComponentsComponent],
     imports: [CommonModule, SharedModule]
 })
 export class PagingAndStateModule {
