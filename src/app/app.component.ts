93e899ce80f5a941c766700e52fb4ed20b30e14d
Fedor Shchudlo <fshchudlo@gmail.com>
2016-09-30 18:33:49 +0700

    reorganize styles for app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 import 'rxjs/Rx';
 
 import { AirportsService } from './shared';
 
 @Component({
-  encapsulation: ViewEncapsulation.None,
   providers: [AirportsService],
   selector: 'rt-demo-app',
   styleUrls: ['app.component.scss'],
