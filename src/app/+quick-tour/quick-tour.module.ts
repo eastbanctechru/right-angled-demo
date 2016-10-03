32190dedd6b14ee8dab33724f6148088c621b384
Fedor Shchudlo <fshchudlo@gmail.com>
2016-10-03 21:02:58 +0700

    use-controls sample

--- src/app/+quick-tour/quick-tour.module.ts
+++ src/app/+quick-tour/quick-tour.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
 import { QuickTourComponent } from './quick-tour.component';
+import { UseControlsComponent } from './use-controls/use-controls.component';
 import { UseListComponent } from './use-list/use-list.component';
 import { UseSelectionComponent } from './use-selection/use-selection.component';
 import { UseSortingsComponent } from './use-sortings/use-sortings.component';
@@ -11,8 +12,8 @@ import { UseStateComponent } from './use-state/use-state.component';
 
 
 @NgModule({
-    declarations: [QuickTourComponent, UseListComponent, UseSortingsComponent, UseStateComponent, UseSelectionComponent],
-    exports: [QuickTourComponent, UseListComponent, UseSortingsComponent, UseStateComponent, UseSelectionComponent],
+    declarations: [QuickTourComponent, UseListComponent, UseControlsComponent, UseSortingsComponent, UseStateComponent, UseSelectionComponent],
+    exports: [QuickTourComponent, UseListComponent, UseControlsComponent, UseSortingsComponent, UseStateComponent, UseSelectionComponent],
     imports: [CommonModule, SharedModule, RouterModule]
 })
 export class QuickTourModule {
---
+++ src/app/+quick-tour/use-controls/use-controls.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { Airport, AirportsListRequest, AirportsService } from '../../shared';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+    selector: 'rt-demo-use-controls',
+    templateUrl: 'use-controls.component.html'
+})
+export class UseControlsComponent {
+    constructor(public airportsService: AirportsService) {
+    }
+    getAirports = (request: AirportsListRequest): Observable<Airport[]> => {
+        return this.airportsService.getAirportsList(request);
+    }
+}
