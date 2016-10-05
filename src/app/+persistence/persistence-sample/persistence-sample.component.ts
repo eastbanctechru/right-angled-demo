332e17be666f9e4fe19c5a95f6581481dd3123ca
Fedor Shchudlo <fshchudlo@gmail.com>
2016-10-05 13:29:37 +0700

    persistence page fixed

--- src/app/+persistence/persistence-sample/persistence-sample.component.ts
+++ src/app/+persistence/persistence-sample/persistence-sample.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ListComponent } from 'right-angled';
 
-import { AirportsListRequest, AirportsService } from '../../shared';
+import { AirportsListRequest, AirportsService, LocalStorageStateService, QueryStringStateService, SessionStorageStateService } from '../../shared';
 
 @Component({
+    providers: [LocalStorageStateService, QueryStringStateService, SessionStorageStateService],
     selector: 'rt-demo-persistence-sample',
     templateUrl: 'persistence-sample.component.html'
 })
-export class PersistenceSampleComponent {
-    constructor(private airportsService: AirportsService) {
+export class PersistenceSampleComponent implements AfterViewInit {
+    @ViewChild(ListComponent) public listComponent: ListComponent;
+    constructor(private airportsService: AirportsService, private queryStringStateService: QueryStringStateService, private localStorageStateService: LocalStorageStateService, private sessionStorageStateService: SessionStorageStateService) {
         this.airportsService = airportsService;
     }
     public loadData = (requestParams: AirportsListRequest): any => {
         return this.airportsService.getAirportsList(requestParams);
     };
+    ngAfterViewInit(): void {
+        this.listComponent.listService.registerFilterTarget(this);
+        this.listComponent.listService.registerStateService(this.localStorageStateService, this.sessionStorageStateService, this.queryStringStateService);
+    }
+
 }
