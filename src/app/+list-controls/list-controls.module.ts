d354ea750bf31939237bcce7fffbd167ec840b05
Fedor Shchudlo <fshchudlo@gmail.com>
2016-10-15 12:40:05 +0700

    deps update

--- src/app/+list-controls/list-controls.module.ts
+++ src/app/+list-controls/list-controls.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
 import { ListControlsComponent } from './list-controls.component';
@@ -7,7 +8,7 @@ import { ListControlsComponent } from './list-controls.component';
 @NgModule({
     declarations: [ListControlsComponent],
     exports: [ListControlsComponent],
-    imports: [CommonModule, SharedModule]
+    imports: [CommonModule, SharedModule, RouterModule]
 })
 export class ListControlsModule {
 }
