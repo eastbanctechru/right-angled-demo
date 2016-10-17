6746d290f285628c7c0be1da9f7ab5ddbc9ba7bc
Fedor Shchudlo <fshchudlo@gmail.com>
2016-10-17 11:55:55 +0700

    `rt-row-number` sample

--- src/app/+list-controls/list-controls.module.ts
+++ src/app/+list-controls/list-controls.module.ts
@@ -5,11 +5,12 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { ListControlsComponent } from './list-controls.component';
 
+import { UseRowNumberComponent } from './use-row-number/use-row-number.component';
 import { UseRtListComponent } from './use-rt-list/use-rt-list.component';
 
 @NgModule({
-    declarations: [ListControlsComponent, UseRtListComponent],
-    exports: [ListControlsComponent, UseRtListComponent],
+    declarations: [ListControlsComponent, UseRtListComponent, UseRowNumberComponent],
+    exports: [ListControlsComponent, UseRtListComponent, UseRowNumberComponent],
     imports: [CommonModule, SharedModule, RouterModule]
 })
 export class ListControlsModule {
---
+++ src/app/+list-controls/use-row-number/use-row-number.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { AirportsPagedListRequest, AirportsService, ListResponse } from '../../shared';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+    selector: 'rt-demo-use-row-number',
+    templateUrl: 'use-row-number.component.html'
+})
+export class UseRowNumberComponent {
+    constructor(private airportsService: AirportsService) {
+    }
+    getAirports = (request: AirportsPagedListRequest): Observable<ListResponse> => {
+        return this.airportsService.getAirportsPagedList(request);
+    }
+}
