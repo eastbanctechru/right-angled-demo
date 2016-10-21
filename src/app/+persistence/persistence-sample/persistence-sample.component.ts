a32c6c2138b69aba7ff316d1a52f960128e9833e
Fedor Shchudlo <fshchudlo@gmail.com>
2016-10-21 12:05:13 +0700

    some list docs

--- src/app/+persistence/persistence-sample/persistence-sample.component.ts
+++ src/app/+persistence/persistence-sample/persistence-sample.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { ListDirective } from 'right-angled';
+import { Component } from '@angular/core';
+import { RtList } from 'right-angled';
 
 import { AirportsListRequest, AirportsService, LocalStorageStateService, QueryStringStateService, SessionStorageStateService } from '../../shared';
 
@@ -9,17 +9,15 @@ import { AirportsListRequest, AirportsService, LocalStorageStateService, QuerySt
     styleUrls: ['persistence-sample.component.scss'],
     templateUrl: 'persistence-sample.component.html'
 })
-export class PersistenceSampleComponent implements AfterViewInit {
-    @ViewChild(ListDirective) public listDirective: ListDirective;
+export class PersistenceSampleComponent {
     constructor(private airportsService: AirportsService, private queryStringStateService: QueryStringStateService, private localStorageStateService: LocalStorageStateService, private sessionStorageStateService: SessionStorageStateService) {
         this.airportsService = airportsService;
     }
     public loadData = (requestParams: AirportsListRequest): any => {
         return this.airportsService.getAirportsList(requestParams);
     };
-    ngAfterViewInit(): void {
-        this.listDirective.listService.registerFilterTarget(this);
-        this.listDirective.listService.registerStateService(this.localStorageStateService, this.sessionStorageStateService, this.queryStringStateService);
+    onListInit(list: RtList): void {
+        list.registerFilterTarget(this);
+        list.registerStateService(this.localStorageStateService, this.sessionStorageStateService, this.queryStringStateService);
     }
-
 }
