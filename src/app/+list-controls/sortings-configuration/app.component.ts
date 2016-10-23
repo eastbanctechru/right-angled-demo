def3ba41e4bf08ffee3853b16a968e1fcdd48232
Fedor Shchudlo <fshchudlo@gmail.com>
2016-10-23 15:48:26 +0700

    rtSort settings docs

---
+++ src/app/+list-controls/sortings-configuration/app.component.ts
@@ -0,0 +1,6 @@
+import { SortDirective } from 'right-angled';
+
+SortDirective.settings.sortableClassName = 'custom-class';
+SortDirective.settings.sortAscClassName = 'asc-custom-class';
+SortDirective.settings.sortDescClassName = 'desc-custom-class';
+export class AppComponent { }
