3497e3d6315d892b602699cdf15ec61b6aa2ba20
Fedor Shchudlo <fshchudlo@gmail.com>
2016-10-24 21:34:50 +0700

    regular lists used in paging section

--- src/app/+paging-and-state/paging-and-state.component.ts
+++ src/app/+paging-and-state/paging-and-state.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  styleUrls: ['paging-and-state.component.scss'],
   templateUrl: 'paging-and-state.component.html'
 })
 export class PagingAndStateComponent {
