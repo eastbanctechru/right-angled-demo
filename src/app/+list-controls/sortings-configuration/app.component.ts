3b7dc116a4e8f3d1e0096dc05721b9adff239e0a
Fedor Shchudlo <fshchudlo@gmail.com>
2016-10-25 18:56:35 +0700

    list controls section verified

--- src/app/+list-controls/sortings-configuration/app.component.ts
+++ src/app/+list-controls/sortings-configuration/app.component.ts
@@ -3,4 +3,5 @@ import { SortDirective } from 'right-angled';
 SortDirective.settings.sortableClassName = 'custom-class';
 SortDirective.settings.sortAscClassName = 'asc-custom-class';
 SortDirective.settings.sortDescClassName = 'desc-custom-class';
+
 export class AppComponent { }
