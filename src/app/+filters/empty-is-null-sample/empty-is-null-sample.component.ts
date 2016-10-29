5dd255c802434f4863e1a539fe7541b218c8a730
Fedor Shchudlo <fshchudlo@gmail.com>
2016-10-29 17:29:18 +0700

    emptyIsNull sample

---
+++ src/app/+filters/empty-is-null-sample/empty-is-null-sample.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FilterConfig, RtFiltersService, filter } from 'right-angled';
+
+@Component({
+  providers: [RtFiltersService],
+  selector: 'rt-demo-empty-is-null-sample',
+  templateUrl: 'empty-is-null-sample.component.html'
+})
+export class EmptyIsNullSampleComponent {
+  public lastRequest: any = '';
+
+  @filter public defaultField: string = '';
+  @filter(<FilterConfig>{ emptyIsNull: true }) public configuredField: string = '';
+
+  constructor(private filtersService: RtFiltersService) {
+    filtersService.registerFilterTarget(this);
+    this.lastRequest = this.filtersService.getRequestState();
+  }
+  public serializeRequest(): void {
+    this.lastRequest = this.filtersService.getRequestState();
+  }
+  public resetSate(): void {
+    this.filtersService.resetValues();
+  }
+}
--- src/app/+filters/filters.module.ts
+++ src/app/+filters/filters.module.ts
@@ -4,13 +4,15 @@ import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
 import { CoerceSampleComponent } from './coerce-sample/coerce-sample.component';
+import { DefaultValueSampleComponent } from './default-value-sample/default-value-sample.component';
+import { EmptyIsNullSampleComponent } from './empty-is-null-sample/empty-is-null-sample.component';
 import { FiltersSampleComponent } from './filters-sample/filters-sample.component';
 import { FiltersComponent } from './filters.component';
 import { ParameterNameSampleComponent } from './parameter-name-sample/parameter-name-sample.component';
 
 @NgModule({
-  declarations: [FiltersComponent, FiltersSampleComponent, CoerceSampleComponent, ParameterNameSampleComponent],
-  exports: [FiltersComponent, FiltersSampleComponent, CoerceSampleComponent, ParameterNameSampleComponent],
+  declarations: [FiltersComponent, FiltersSampleComponent, CoerceSampleComponent, DefaultValueSampleComponent, EmptyIsNullSampleComponent, ParameterNameSampleComponent],
+  exports: [FiltersComponent, FiltersSampleComponent, CoerceSampleComponent, DefaultValueSampleComponent, EmptyIsNullSampleComponent, ParameterNameSampleComponent],
   imports: [CommonModule, SharedModule, RouterModule]
 })
 export class FiltersModule {
