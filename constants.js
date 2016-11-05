ffbf4db78980141450abd8e8a1b0aa98d7e338a7
Fedor Shchudlo <fshchudlo@gmail.com>
2016-11-06 00:40:27 +0700

    scripts for gh-pages deploy

--- constants.js
+++ constants.js
@@ -54,6 +54,7 @@ exports.MY_COPY_FOLDERS = [
   // use this for folders you want to be copied in to Client dist
   // src/assets and index.html are already copied by default.
   // format is { from: 'folder_name', to: 'folder_name' }
+  { from: '.gitignore' },
   { from: 'src/favicon.ico' },
   { from: 'src/404.html' },
   { from: 'node_modules/bootstrap/dist/css/bootstrap.min.css' },
