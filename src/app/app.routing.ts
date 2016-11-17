8579788c58c99f83efb34500ba1f92b15859b270
Fedor Shchudlo <fshchudlo@gmail.com>
2016-11-17 16:54:44 +0700

    "+" sign removed from lazy routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,12 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { QuickTourRoutes } from './quick-tour/quick-tour.routes';
 
 export const appRoutes: Routes = [
-  { loadChildren: '+filters/filters.module#FiltersModule', path: 'filters' },
-  { loadChildren: '+list-controls/list-controls.module#ListControlsModule', path: 'list-controls' },
-  { loadChildren: '+misc-directives/misc-directives.module#MiscDirectivesModule', path: 'misc-directives' },
-  { loadChildren: '+paging-and-state/paging-and-state.module#PagingAndStateModule', path: 'paging-and-state' },
-  { loadChildren: '+selection/selection.module#SelectionModule', path: 'selection' },
-  { loadChildren: '+persistence/persistence.module#PersistenceModule', path: 'persistence' },
+  { loadChildren: 'filters/filters.module#FiltersModule', path: 'filters' },
+  { loadChildren: 'list-controls/list-controls.module#ListControlsModule', path: 'list-controls' },
+  { loadChildren: 'misc-directives/misc-directives.module#MiscDirectivesModule', path: 'misc-directives' },
+  { loadChildren: 'paging-and-state/paging-and-state.module#PagingAndStateModule', path: 'paging-and-state' },
+  { loadChildren: 'selection/selection.module#SelectionModule', path: 'selection' },
+  { loadChildren: 'persistence/persistence.module#PersistenceModule', path: 'persistence' },
   ...QuickTourRoutes,
   { path: '**', redirectTo: '' }
 ];
