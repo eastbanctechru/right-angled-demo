cec887f1ef4501d7a7a02f0ad880a6bb03e81606
Fedor Shchudlo <fshchudlo@gmail.com>
2016-11-30 12:38:55 +0700

    chore(all): deps update

--- src/app/app.providers.ts
+++ src/app/app.providers.ts
@@ -1,3 +1,1 @@
-import { Angulartics2GoogleAnalytics } from 'angulartics2/dist/providers/ga/angulartics2-ga';
-
-export const APP_PROVIDERS = [Angulartics2GoogleAnalytics];
+export const APP_PROVIDERS = [];
