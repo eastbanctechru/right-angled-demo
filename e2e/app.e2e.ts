9a724c923f8bf4e4ad146f89c56bd23d6b76c0c4
Fedor Shchudlo <fshchudlo@gmail.com>
2016-11-30 21:34:40 +0700

    small text fixes

--- e2e/app.e2e.ts
+++ e2e/app.e2e.ts
@@ -6,7 +6,7 @@ describe('App', () => {
 
   it('should have a title', () => {
     let subject = browser.getTitle();
-    let result  = 'right-angled - building kit for angular2 data grids';
+    let result  = 'right-angled - lightweight building kit for angular2 data grids';
     expect(subject).toEqual(result);
   });
 
