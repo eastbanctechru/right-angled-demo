b0718ebce4d87ae06b1a6b5aca054ac980b3e93f
Fedor Shchudlo <fshchudlo@gmail.com>
2016-12-16 12:32:43 +0700

    remove unnecessary sections from quick tour

--- src/app/quick-tour/quick-tour.module.ts
+++ src/app/quick-tour/quick-tour.module.ts
@@ -5,7 +5,6 @@ import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
 import { QuickTourComponent } from './quick-tour.component';
-import { StarterComponent } from './starter/starter.component';
 import { UseControlsComponent } from './use-controls/use-controls.component';
 import { UseFiltersComponent } from './use-filters/use-filters.component';
 import { UseListComponent } from './use-list/use-list.component';
@@ -16,8 +15,8 @@ import { UseSortingsComponent } from './use-sortings/use-sortings.component';
 import { UseStatusesComponent } from './use-statuses/use-statuses.component';
 
 @NgModule({
-  declarations: [QuickTourComponent, StarterComponent, UseListComponent, UseControlsComponent, UseFiltersComponent, UsePagingComponent, UsePersistenceComponent, UseSortingsComponent, UseStatusesComponent, UseSelectionComponent],
-  exports: [QuickTourComponent, StarterComponent, UseListComponent, UseControlsComponent, UseFiltersComponent, UsePagingComponent, UsePersistenceComponent, UseSortingsComponent, UseStatusesComponent, UseSelectionComponent],
+  declarations: [QuickTourComponent, UseListComponent, UseControlsComponent, UseFiltersComponent, UsePagingComponent, UsePersistenceComponent, UseSortingsComponent, UseStatusesComponent, UseSelectionComponent],
+  exports: [QuickTourComponent, UseListComponent, UseControlsComponent, UseFiltersComponent, UsePagingComponent, UsePersistenceComponent, UseSortingsComponent, UseStatusesComponent, UseSelectionComponent],
   imports: [CommonModule, SharedModule, FormsModule, RouterModule]
 })
 export class QuickTourModule {
--- src/app/quick-tour/starter/starter.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'rt-demo-starter',
-  templateUrl: 'starter.component.html'
-})
-export class StarterComponent {
-}
