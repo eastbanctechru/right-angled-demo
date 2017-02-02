ea5b20fc5f5e8c479cdfd62ff794340191ad4565
Fedor Shchudlo <fshchudlo@gmail.com>
2017-02-02 14:07:51 +0700

    deps update

--- webpack.config.ts
+++ webpack.config.ts
@@ -19,7 +19,7 @@ const {
   DllPlugin,
   DllReferencePlugin,
   ProgressPlugin,
-  NoErrorsPlugin
+  NoEmitOnErrorsPlugin
 } = require('webpack');
 
 const CompressionPlugin = require('compression-webpack-plugin');
@@ -161,7 +161,7 @@ const clientConfig = function webpackConfig(): WebpackConfig {
 
   if (PROD) {
     config.plugins.push(
-      new NoErrorsPlugin(),
+      new NoEmitOnErrorsPlugin(),
       new UglifyJsPlugin({
         beautify: false,
         comments: false
