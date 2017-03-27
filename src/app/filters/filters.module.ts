426dd71b9e695c4c8a83a6b8256b8c6002b8adc8
f.shchudlo <fshchudlo@gmail.com>
2017-03-27 15:59:12 +0700

    omitIfNullOrUndefined description added

--- src/app/filters/filters.module.ts
+++ src/app/filters/filters.module.ts
@@ -10,12 +10,13 @@ import { FiltersSampleComponent } from './filters-sample/filters-sample.componen
 import { FiltersComponent } from './filters.component';
 import { FiltersRoutes } from './filters.routes';
 import { IgnoreOnAutoMapSampleComponent } from './ignore-on-auto-map-sample/ignore-on-auto-map-sample.component';
+import { OmitIfNullSampleComponent } from './omit-if-null-or-undefined-sample/omit-if-null-or-undefined-sample.component';
 import { ParameterNameSampleComponent } from './parameter-name-sample/parameter-name-sample.component';
 import { RegisterAsFilterSampleComponent } from './register-as-filter-sample/register-as-filter-sample.component';
 
 @NgModule({
-  declarations: [FiltersComponent, FiltersSampleComponent, CoerceSampleComponent, DefaultValueSampleComponent, EmptyIsNullSampleComponent, IgnoreOnAutoMapSampleComponent, ParameterNameSampleComponent, RegisterAsFilterSampleComponent],
-  exports: [FiltersComponent, FiltersSampleComponent, CoerceSampleComponent, DefaultValueSampleComponent, EmptyIsNullSampleComponent, IgnoreOnAutoMapSampleComponent, ParameterNameSampleComponent, RegisterAsFilterSampleComponent],
+  declarations: [FiltersComponent, FiltersSampleComponent, CoerceSampleComponent, DefaultValueSampleComponent, EmptyIsNullSampleComponent, IgnoreOnAutoMapSampleComponent, OmitIfNullSampleComponent, ParameterNameSampleComponent, RegisterAsFilterSampleComponent],
+  exports: [FiltersComponent, FiltersSampleComponent, CoerceSampleComponent, DefaultValueSampleComponent, EmptyIsNullSampleComponent, IgnoreOnAutoMapSampleComponent, OmitIfNullSampleComponent, ParameterNameSampleComponent, RegisterAsFilterSampleComponent],
   imports: [CommonModule, SharedModule, RouterModule.forChild(FiltersRoutes)]
 })
 export class FiltersModule {
