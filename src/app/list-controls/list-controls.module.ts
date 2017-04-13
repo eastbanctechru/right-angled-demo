487fbed92a74414ceac928b29d9cd1fd0cd6d8f8
f.shchudlo <fshchudlo@gmail.com>
2017-04-13 15:29:34 +0700

    remove unused components

--- src/app/list-controls/list-controls.module.ts
+++ src/app/list-controls/list-controls.module.ts
@@ -6,7 +6,6 @@ import { SharedModule } from '../shared/shared.module';
 import { ListControlsComponent } from './list-controls.component';
 import { ListControlsRoutes } from './list-controls.routes';
 
-import { CustomItemsHandlingComponent } from './custom-items-handling/custom-items-handling.component';
 import { DefaultSortingsComponent } from './default-sortings/default-sortings.component';
 import { KeepRecordsOnLoadComponent } from './keep-records-on-load/keep-records-on-load.component';
 import { UseRowNumberComponent } from './use-row-number/use-row-number.component';
@@ -14,8 +13,8 @@ import { UseRTListComponent } from './use-rt-list/use-rt-list.component';
 import { UseSortingsComponent } from './use-sortings/use-sortings.component';
 
 @NgModule({
-  declarations: [ListControlsComponent, CustomItemsHandlingComponent, DefaultSortingsComponent, KeepRecordsOnLoadComponent, UseRTListComponent, UseRowNumberComponent, UseSortingsComponent],
-  exports: [ListControlsComponent, CustomItemsHandlingComponent, DefaultSortingsComponent, KeepRecordsOnLoadComponent, UseRTListComponent, UseRowNumberComponent, UseSortingsComponent],
+  declarations: [ListControlsComponent, DefaultSortingsComponent, KeepRecordsOnLoadComponent, UseRTListComponent, UseRowNumberComponent, UseSortingsComponent],
+  exports: [ListControlsComponent, DefaultSortingsComponent, KeepRecordsOnLoadComponent, UseRTListComponent, UseRowNumberComponent, UseSortingsComponent],
   imports: [CommonModule, SharedModule, RouterModule.forChild(ListControlsRoutes)]
 })
 export class ListControlsModule {
