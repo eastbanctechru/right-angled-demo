6ccb3796ea4b9895d7bb0026c4f08a25fa9df5f1
f.shchudlo <fshchudlo@gmail.com>
2017-07-02 18:22:04 +0700

    formatting

--- prodserver.js
+++ prodserver.js
@@ -1,29 +1,31 @@
 "use strict";
-const compression = require('compression')
-const express = require('express'),
-path = require('path');
+const compression = require("compression");
+const express = require("express"),
+    path = require("path");
 
-const E2E_PORT = require('./constants').E2E_PORT;
-const HOST = require('./constants').HOST;
-const PROD_PORT = require('./constants').PROD_PORT;
+const E2E_PORT = require("./constants").E2E_PORT;
+const HOST = require("./constants").HOST;
+const PROD_PORT = require("./constants").PROD_PORT;
 
 const app = express();
-const ROOT = path.join(path.resolve(__dirname, '..'));
+const ROOT = path.join(path.resolve(__dirname, ".."));
 
 app.use(compression());
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 
 const renderIndex = (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
-}
+    res.sendFile(path.resolve(__dirname, "dist/index.html"));
+};
 
-app.get('/*', renderIndex);
+app.get("/*", renderIndex);
 
 let e2e;
 const ENV = process.env.npm_lifecycle_event;
-if (ENV === 'e2e:server') { e2e = E2E_PORT };
+if (ENV === "e2e:server") {
+    e2e = E2E_PORT;
+}
 const PORT = e2e || PROD_PORT;
 
 app.listen(PORT, () => {
-  console.log(`Listening on: http://${HOST}:${PORT}`);
+    console.log(`Listening on: http://${HOST}:${PORT}`);
 });
